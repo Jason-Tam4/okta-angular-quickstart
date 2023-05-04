7e66f5bda8e4f723a432a183021304fa2d6f2bfc
Jason Tam <jason.tam@vanta.com>
2023-05-03 17:10:00 -0700

    temp?

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OktaAuthModule, OKTA_CONFIG } from '@okta/okta-angular';
+import { OktaAuth } from '@okta/okta-auth-js';
+import { ProfileComponent } from './profile/profile.component';
+
+const oktaAuth = new OktaAuth({
+  issuer: 'https://dev-70300601.okta.com/oauth2/default',
+  clientId: '0oa9dsef1rENsAHm05d7',
+  redirectUri: window.location.origin + '/login/callback'
+});
+OktaAuthModule.forRoot({ oktaAuth })
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	  OktaAuthModule
   ],
-  providers: [],
+  providers: [
+    { provide: OKTA_CONFIG, useValue: { oktaAuth } }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
